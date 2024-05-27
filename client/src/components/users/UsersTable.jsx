346d79f5c79e774499f761305e617aedeb3c3fef
Tzivi Shachar <tzivish2141@gmail.com>
2024-05-27 20:46:57 +0300

    users

---
+++ client/src/components/users/UsersTable.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { getRequest } from '../../modules/requests/server_requests';
+const UsersTable = ({token}) => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const responseData = await getRequest('http://localhost:3000/users',token);
+                if(responseData.ok){
+                    await setUsers(responseData.body);
+                }
+            } catch (error) {
+                alert('ישנה בעיה, בבקשה נסה שוב');
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>שם</th>
+                    <th>אמייל</th>
+                    <th>עיר</th>
+                    <th>רחוב</th>
+                    <th>מספר בית</th>
+                    <th>שם העסק</th>
+                    <th>מספר טלפון 1</th>
+                    <th>מספר טלפון 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.city}</td>
+                        <td>{user.street}</td>
+                        <td>{user.houseNumber}</td>
+                        <td>{user.username}</td>
+                        <td>{user.phone1}</td>
+                        <td>{user.phone2}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default UsersTable;
