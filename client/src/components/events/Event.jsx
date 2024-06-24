3c3f569d7ed41b435457e92c10ed225420b15237
Tzivi Shachar <tzivish2141@gmail.com>
2024-06-24 17:34:47 +0300

    order changes

--- client/src/components/events/Event.jsx
+++ client/src/components/events/Event.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import '../../css/event.css';
+import formatDates from '../../modules/formatDateTime'
 
 function Event({ event }) {
   return (
     <div className="event">
       <p>{event.text}</p>
-      <p>תאריך: {event.date}</p>
+      <p>תאריך: {formatDates.formatDateTime(event.date)}</p>
     </div>
   );
 }
