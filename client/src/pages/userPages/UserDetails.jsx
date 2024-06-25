d279ef88bb96528109be2dc09ee61c32bcffdb83
Tzivi Shachar <tzivish2141@gmail.com>
2024-06-25 17:46:48 +0300

    jhkvyuy

--- client/src/pages/userPages/UserDetails.jsx
+++ client/src/pages/userPages/UserDetails.jsx
@@ -5,10 +5,9 @@ import '../../css/usersDetails.css';
 import Loading from '../../components/Loading';
 import { useDispatch, useSelector } from 'react-redux';
 
-
 const UserDetails = () => {
   const [user, setUser] = useState({});
-  const [email, setEmail] = useState({});
+  const [email, setEmail] = useState('');
   const [alert, setAlert] = useState('');
   const [isEditing, setIsEditing] = useState(false);
   const [loading, setLoading] = useState(true);
@@ -20,7 +19,6 @@ const UserDetails = () => {
   const dispatch = useDispatch();
   const token = useSelector(state => state.app.token);
   const current_user = useSelector(state => state.app.user);
-
 
   useEffect(() => {
     async function getUsersDetails() {
@@ -80,43 +78,53 @@ const UserDetails = () => {
     <div className='usersbody'>
       {loading && <Loading />}
       {alert && <h2>{alert}</h2>}
+
+     
+
       {isEditing ? (
-        <div className='editForm_container'>
-          <div className='editForm_form'>
-            <label>שם: <input className='editInput' type="text" name="name" value={user.name} onChange={handleChange} /></label>
-            <label>אמייל: <input className='editInput' type="email" name="email" value={user.email} onChange={handleChange} /></label>
-            <label>עיר: <input className='editInput' type="text" name="city" value={user.city} onChange={handleChange} /></label>
-            <label>רחוב: <input className='editInput' type="text" name="street" value={user.street} onChange={handleChange} /></label>
-            <label>מספר בית: <input className='editInput' type="text" name="houseNumber" value={user.houseNumber} onChange={handleChange} /></label>
-            <label>שם העסק: <input className='editInput' type="text" name="username" value={user.username} onChange={handleChange} /></label>
-            <label>מספר טלפון 1: <input className='editInput' type="text" name="phone1" value={user.phone1} onChange={handleChange} /></label>
-            <label>מספר טלפון 2: <input className='editInput' type="text" name="phone2" value={user.phone2} onChange={handleChange} /></label>
-            <button className='editButton' onClick={handleSave}>שמירה</button>
-            <button className='editButton' onClick={() => setIsEditing(false)}>ביטול</button>
+        <div >
+          <div className='user_section edit_user'>
+            <label>שם: </label><br></br><input className='input_field' type="text" name="name" value={user.name} onChange={handleChange} /><br></br>
+            <label>אמייל: </label><br></br><input className='input_field' type="email" name="email" value={user.email} onChange={handleChange} /><br></br>
+            <label>עיר: </label><br></br><input className='input_field' type="text" name="city" value={user.city} onChange={handleChange} /><br></br>
+            <label>רחוב:</label> <br></br><input className='input_field' type="text" name="street" value={user.street} onChange={handleChange} /><br></br>
+            <label>מספר בית: </label><input className='input_field' type="text" name="houseNumber" value={user.houseNumber} onChange={handleChange} /><br></br>
+            <label>שם העסק: </label><input className='input_field' type="text" name="username" value={user.username} onChange={handleChange} /><br></br>
+            <label>מספר טלפון 1: </label><input className='input_field' type="text" name="phone1" value={user.phone1} onChange={handleChange} /><br></br>
+            <label>מספר טלפון 2: </label><input className='input_field' type="text" name="phone2" value={user.phone2} onChange={handleChange} />
+            <button className='button' onClick={handleSave}>שמירה</button>
+            <button className='button' onClick={() => setIsEditing(false)}>ביטול</button>
           </div>
         </div>
       ) : (
-        <div className='user_information'>
-          <h2>פרטי משתמש</h2>
-          <p><strong>שם: <FontAwesomeIcon icon="fas fa-user" /></strong> {user.name}</p>
-          <p><strong>אמייל: <FontAwesomeIcon icon="fas fa-envelope" /></strong> {user.email}</p>
-          <p><strong>עיר: <FontAwesomeIcon icon="fas fa-city" /></strong> {user.city}</p>
-          <p><strong>רחוב: <FontAwesomeIcon icon="fas fa-road" /></strong> {user.street}</p>
-          <p><strong>מספר בית: <FontAwesomeIcon icon="fas fa-house-user" /></strong> {user.houseNumber}</p>
-          <p><strong>שם העסק: <FontAwesomeIcon icon="fas fa-user-tie" /></strong> {user.username}</p>
-          <p><strong>מספר טלפון 1: <FontAwesomeIcon icon="fas fa-phone" /></strong> {user.phone1}</p>
-          <p><strong>מספר טלפון 2: <FontAwesomeIcon icon="fas fa-phone" /></strong> {user.phone2}</p>
-          <button className='editButton' onClick={() => setIsEditing(true)}>עריכה</button>
+        <div className='user_section'>
+          <div className='user_information'>
+            <h2>פרטי משתמש</h2>
+            <p><strong>שם: <FontAwesomeIcon icon="fas fa-user" /></strong> {user.name}</p>
+            <p><strong>אמייל: <FontAwesomeIcon icon="fas fa-envelope" /></strong> {user.email}</p>
+            <p><strong>עיר: <FontAwesomeIcon icon="fas fa-city" /></strong> {user.city}</p>
+            <p><strong>רחוב: <FontAwesomeIcon icon="fas fa-road" /></strong> {user.street}</p>
+            <p><strong>מספר בית: <FontAwesomeIcon icon="fas fa-house-user" /></strong> {user.houseNumber}</p>
+            <p><strong>שם העסק: <FontAwesomeIcon icon="fas fa-user-tie" /></strong> {user.username}</p>
+            <p><strong>מספר טלפון 1: <FontAwesomeIcon icon="fas fa-phone" /></strong> {user.phone1}</p>
+            <p><strong>מספר טלפון 2: <FontAwesomeIcon icon="fas fa-phone" /></strong> {user.phone2}</p>
+            <button className='button' onClick={() => setIsEditing(true)}>עריכה</button>
+          </div>
         </div>
       )}
-      <div className='changePassword'>
-        <h3>שנה סיסמא</h3>
-        <label>סיסמא נוכחית: <input className='passwordInput' type="password" name="password" value={passwords.password} onChange={handlePasswordChange} /></label>
-        <label>סיסמא חדשה: <input className='passwordInput' type="password" name="newPassword" value={passwords.newPassword} onChange={handlePasswordChange} /></label>
-        <label>חזור על הסיסמא החדשה: <input className='passwordInput' type="password" name="confirmNewPassword" value={passwords.confirmNewPassword} onChange={handlePasswordChange} /></label>
-        <button onClick={handleSavePassword}>שמור סיסמא</button>
-      </div>
+   
+
+      
+  <div className='user_section'>
+    <div className='changePassword'>
+      <h3>שנה סיסמא</h3>
+      <label>סיסמא נוכחית:</label><input className='input_field' type="password" name="password" value={passwords.password} onChange={handlePasswordChange} /><br></br>
+      <label>סיסמא חדשה:</label><input className='input_field' type="password" name="newPassword" value={passwords.newPassword} onChange={handlePasswordChange} /><br></br>
+      <label>חזור על הסיסמא החדשה: </label><input className='input_field' type="password" name="confirmNewPassword" value={passwords.confirmNewPassword} onChange={handlePasswordChange} />
+      <button className='button' onClick={handleSavePassword}>שמור סיסמא</button>
     </div>
+  </div>
+    </div >
   );
 };
 
